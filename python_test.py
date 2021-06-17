77476a6836de6b625ff52f44a7708717eaa9cd1a
Jane-Nyambura <79199503+Jane-Nyambura@users.noreply.github.com>
2021-06-17 10:55:22 +0300

    Add files via upload

---
+++ python_test.py
@@ -0,0 +1,50 @@
+x=[100,110,120,130,140,150]
+z=[]
+for y in x:
+    z=y*5
+print(z)
+    
+# def divisible_by_three(*args):
+#     total=1
+# for numbers in args:
+#         total%3==0
+#     return total    
+# divisible_by_three()
+
+
+def flattens(x):
+    x=[[1,2],[3,4],[5,6]]
+    return x.append
+flattens(x)
+
+def smallest(x):
+    x=[134,34,56,356,3,7,8,4]
+
+x=['a','b','a','e','d','b','c','e','f','g','h']
+x.keys()
+x={'a','b','a','e','d','b','c','e','f','g','h'}
+y={'a','b','a','e','d','b','c',}
+x.union(y)
+x.difference(y)
+
+def divisible_by_seven():
+     range(100,200)
+for k in range:
+        k%7==0
+print (k)
+    
+age=19
+name="Eunice"
+print(f"Hello, you were {age} and your name is{name}")
+students = [{"age": 19, "name": "Eunice"}, {"age": 21, "name": "Agnes"}, {"age": 18, "name": "Teresa"}, {"age": 22, "name": "Asha"}],
+
+
+class  Rectangle:
+    def __init__(self,width,length):
+        self.width=width,
+        self.length=length
+    def area(self,amount):
+        
+
+    
+
